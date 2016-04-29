c377758bdd75e391982fef27cc71cc75e38d5cc0
Humberto S Lourenco <betto@m3uzz.com>
2016-04-29 13:18:08 -0300

    Base

--- dist/js/vegas.conf.js
+++ dist/js/vegas.conf.js
@@ -1,5 +1,12 @@
+var bgVegas = [
+    { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/trem.jpg' },
+   	{ src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/salar.jpg' },
+   	{ src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/flamingo.jpg' }
+];
+
 $(document).ready(function() {
 	$('#container').hide();
+
 	$('body').vegas({
 	    delay: 12000,
 	    timer: true,
@@ -9,10 +16,6 @@ $(document).ready(function() {
 	    transition: 'blur2',
 	    animation: 'random',
 	    transitionDuration: 4000,
-	    slides: [
-	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/flamingo.jpg' },
-	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/salar.jpg' },
-	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/trem.jpg' }
-	    ]
+	    slides: bgVegas
 	});
 });
