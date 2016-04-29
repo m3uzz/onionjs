b13368727e485528f6677db7c8c3767244a81966
Humberto S Lourenco <betto@m3uzz.com>
2016-04-28 23:27:51 -0300

    Base

--- dist/js/vegas.conf.js
+++ dist/js/vegas.conf.js
@@ -1,28 +1,18 @@
 $(document).ready(function() {
 	$('#container').hide();
-	$.vegas({
-		src : '/vendor/m3uzz/onionjs-0.16.4/dist/img/flamingo.jpg',
-		fade : 2000,
-		complete : function() {
-			$('#container').fadeIn(1000);
-			$.vegas('slideshow', {
-				delay : 8000,
-				backgrounds : [ {
-					src : '/vendor/m3uzz/onionjs-0.16.4/dist/img/flamingo.jpg',
-					fade : 2000
-				}, {
-					src : '/vendor/m3uzz/onionjs-0.16.4/dist/img/salar.jpg',
-					fade : 2000
-				}, {
-					src : '/vendor/m3uzz/onionjs-0.16.4/dist/img/trem.jpg',
-					fade : 2000
-				}, ]
-			})('overlay', {
-				src : '/vendor/jaysalvat/vegas-2.2.0/dist/overlays/07.png'
-			});
-		}
-	});
-	$.vegas('overlay', {
-		src : '/vendor/jaysalvat/vegas-2.2.0/dist/overlays/07.png'
+	$('body').vegas({
+	    delay: 12000,
+	    timer: true,
+	    color: '#333333',
+	    overlay: '/vendor/jaysalvat/vegas-2.2.0/dist/overlays/07.png',
+	    shuffle: false,
+	    transition: 'blur2',
+	    animation: 'random',
+	    transitionDuration: 4000,
+	    slides: [
+	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/flamingo.jpg' },
+	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/salar.jpg' },
+	        { src: '/vendor/m3uzz/onionjs-0.16.4/dist/img/trem.jpg' }
+	    ]
 	});
 });
